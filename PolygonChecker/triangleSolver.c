fca4a318bc1ed5e1043fcfe2a9362785a20781c3
sxlmons <salmonsjoshuaa@gmail.com>
2023-11-15 19:00:51 -0500

    added the triangle inequality theorom

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -11,6 +11,13 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
 		result = "Not a triangle";
 	}
+	// Check the triangle inequality theorem
+	// The sum of the lengths of any two sides of a triangle must be greater than the length of the third side
+	// If not, it's not a triangle
+	else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) { 
+		result = "Not a triangle"; 
+	} 
+
 	else if (side1 == side2 && side1 == side3) {
 		result = "Equilateral triangle";
 	}
